4ffbd5b2eeb106c58b7d6d53c69e7c78ab669e77
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-26 11:06:28 +0700

    Argument Order Dependency Done

--- car.js
+++ car.js
@@ -3,13 +3,13 @@
 class Car {
 
   start_car(options) {
-    if (options && options['headlights']) {
+    if (options && options.hasOwnProperty(['headlights'])) {
       this.headlights = options['headlights']
     } else {
       this.headlights = true
     }
 
-    if (options && options['gear']) {
+    if (options && options.hasOwnProperty(['gear'])) {
       this.gear = options['gear']
     } else {
       this.gear = 1
@@ -20,7 +20,7 @@ class Car {
 const herbie = new Car()
 
 // When we start the car, the headlights should be on by default
-herbie.start_car()
+herbie.start_car(true)
 
 console.log(herbie.headlights) // true
 
