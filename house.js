91400e739213c3364b90f7001dc5fa06f75d4e28
= <pangkeyd@gmail.com>
2017-09-26 21:24:54 +0700

    argumentOrderDependency-dpangkey

--- house.js
+++ house.js
@@ -1,21 +1,21 @@
-'use strict'
+// 'use strict'
 
 class House {
-
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  // address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants
+  constructor(argumentss) {
+    this.address = argumentss['address']
+    this.square_feet = argumentss['square_feet']
+    this.num_bedrooms = argumentss['num_bedrooms'] || 3
+    this.num_baths = argumentss['num_baths'] || 2
+    this.cost = argumentss['cost'] || 320000
+    this.down_payment = argumentss['down_payment'] || 0.20
+    this.sold = argumentss['sold'] || false
+    this.short_sale = argumentss['short_sale']
+    this.has_tenants = argumentss['has_tenants'] || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -32,7 +32,20 @@ class House {
     return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
   }
 }
+// address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants
+// 'address', 100, 2, 2, 12345, 12345, true, true
+let argumentss = {
+  address : 'address',
+  square_feet : 100,
+  num_bedrooms : 2,
+  num_baths : 2,
+  cost : 12345,
+  down_payment : 12345,
+  sold : true,
+  short_sale : 'short_sale',
+  has_tenants : true
+}
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const cool = new House(argumentss)
 
 console.log(cool.to_s())
