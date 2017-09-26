f694bc96a7e4ca6b443b4e8654c89dffdcf0f08a
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-26 14:33:40 +0700

    baru yang house

--- house.js
+++ house.js
@@ -2,26 +2,28 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(param) {
+    this.address = param['address']
+    this.square_feet = param['square_feet']
+    this.num_bedrooms = param['num_bedrooms'] || 3
+    this.num_baths = param['num_baths'] || 2
+    this.cost = param['cost'] || 320000
+    this.down_payment = param['down_payment'] || 0.20
+    this.sold = param['sold'] || false
+    this.short_sale = param['short_sale']
+    this.has_tenants = param['has_tenants'] || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    let newaddress = this.address.replace(/.{10}$/g, '****')
+    return newaddress
   }
 
   buy(money, good_credit) {
     if (money >= down_payment && good_credit) {
       this.sold = true
     }
+    return this
   }
 
   down_payment() {
@@ -33,6 +35,18 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let obj = {
+  address: 'address',
+  square_feet: 100,
+  num_bedrooms: 2,
+  num_baths: 2,
+  cost: 12345,
+  down_payment: 12345,
+  sold: true,
+  short_sale: true,
+  has_tenants: true
+}
+
+const cool = new House(obj)
 
 console.log(cool.to_s())
