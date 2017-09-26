d3e19669bbd4fc882b4ef0342c9648663f9a5fb0
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-26 10:22:18 +0700

    Commit Release 1

--- house.js
+++ house.js
@@ -3,15 +3,15 @@
 class House {
 
   constructor(components) {
-    this.address = components['address']
-    this.square_feet = components['square_feet']
-    this.num_bedrooms = components['num_bedrooms'] || 3
-    this.num_baths = components['num_baths'] || 2
-    this.cost = components['cost'] || 320000
-    this.down_payment = components['down_payment'] || 0.20
-    this.sold = components['sold'] || false
-    this.short_sale = components['short_sale']
-    this.has_tenants = components['has_tenants'] || false
+    this.address = components.hasOwnProperty('address')?components['address']:'';
+    this.square_feet = components.hasOwnProperty('square_feet')?components['square_feet']:'';
+    this.num_bedrooms = components.hasOwnProperty('num_bedrooms')?components['num_bedrooms']:3;
+    this.num_baths = components.hasOwnProperty('num_baths')?components['num_baths']:2;
+    this.cost = components.hasOwnProperty('cost')?components['cost']:320000;
+    this.down_payment = components.hasOwnProperty('down_payment')?components['down_payment']:0.20;
+    this.sold = components.hasOwnProperty('sold')?components['sold']:false;
+    this.short_sale = components.hasOwnProperty('short_sale')?components['short_sale']:false;
+    this.has_tenants = components.hasOwnProperty('has_tenants')?components['has_tenants']:false;
   }
 
   obscure_address() {
