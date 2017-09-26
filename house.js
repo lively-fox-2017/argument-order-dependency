00cda6e4a5b2f70838de39132ccdc4c02291eec2
nach9 <nur.achdiansyah@gmail.com>
2017-09-26 10:23:27 +0700

    release 1 done

--- house.js
+++ house.js
@@ -1,21 +1,21 @@
 'use strict'
 
 class House {
-
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  // address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants
+  constructor(component) {
+    this.address = component.address
+    this.square_feet = component.square_feet
+    this.num_bedrooms = component.num_bedrooms || 3
+    this.num_baths = component.num_baths || 2
+    this.cost = component.cost || 320000
+    this.down_payment = component.down_payment || 0.20
+    this.sold = component.sold || false
+    this.short_sale = component.short_sale
+    this.has_tenants = component.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return (this.address.replace(/.{10}$/g, '****'))
   }
 
   buy(money, good_credit) {
@@ -33,6 +33,19 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let component = {
+  address: 'address',
+  square_feet: 100,
+  num_bedrooms: 2,
+  num_baths: 2,
+  cost: 12345,
+  down_payment: 0.1,
+  sold: true,
+  short_sale: true,
+  has_tenants: true
+}
+
+// const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const cool = new House(component)
 
 console.log(cool.to_s())
