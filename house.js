743ac4c5d7efa0c05ab4d26e41182d7e02bae59a
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-09-26 11:39:09 +0700

    Release 1

--- house.js
+++ house.js
@@ -2,30 +2,31 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(obj) {
+    this.address = obj.address
+    this.square_feet = obj.square_feet
+    this.num_bedrooms = obj.num_bedrooms || 3
+    this.num_baths = obj.num_baths || 2
+    this.cost = obj.cost || 320000
+    this.down_payment = obj.down_payment || 0.20
+    this.sold = obj.sold || false
+    this.short_sale = obj.short_sale
+    this.has_tenants = obj.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
-    if (money >= down_payment && good_credit) {
+    if (money >= this.down_payment && good_credit) {
       this.sold = true
     }
+    return this.sold
   }
 
-  down_payment() {
-    return cost * this.down_payment
+  dp() { //down_payment
+    return this.cost * this.down_payment
   }
 
   to_s() {
@@ -33,6 +34,32 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+var obj = {
+  address : 'address',
+  square_feet : 100,
+  num_bedrooms : 2,
+  num_baths : 2,
+  cost : 12345,
+  down_payment : 12345,
+  sold : true,
+  short_sale : true,
+  has_tenants : 'has_tenants' || false, // bagaimana cara mengisi nilai default?
+}
 
+var args = obj;
+const cool = new House(args); // instansiasi
+// const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+// console.log(cool.address);
+// console.log(cool.square_feet);
+// console.log(cool.num_bedrooms);
+// console.log(cool.num_baths);
+// console.log(cool.cost);
+// console.log(cool.down_payment);
+// console.log(cool.sold);
+// console.log(cool.short_sale);
+// console.log(cool.has_tenants);
+
+console.log(cool.obscure_address());
+console.log(cool.buy(100, 100));
+console.log(cool.dp());
 console.log(cool.to_s())
