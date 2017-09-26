800257eec88b747e3701329b8619684691b94608
NNcrawler <rasyid.xyz@gmail.com>
2017-09-26 09:56:40 +0700

    house constructor turn to obj

--- house.js
+++ house.js
@@ -2,16 +2,28 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  // constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
+  //   this.address = address
+  //   this.square_feet = square_feet
+  //   this.num_bedrooms = num_bedrooms || 3
+  //   this.num_baths = num_baths || 2
+  //   this.cost = cost || 320000
+  //   this.down_payment = down_payment || 0.20
+  //   this.sold = sold || false
+  //   this.short_sale = short_sale
+  //   this.has_tenants = has_tenants || false
+  // }
+
+  constructor(inputParams) {
+    this.address = inputParams.address
+    this.square_feet = inputParams.square_feet
+    this.num_bedrooms = inputParams.num_bedrooms || 3
+    this.num_baths = inputParams.num_baths || 2
+    this.cost = inputParams.cost || 320000
+    this.down_payment = inputParams.down_payment || 0.20
+    this.sold = inputParams.sold || false
+    this.short_sale = inputParams.short_sale
+    this.has_tenants = inputParams.has_tenants || false
   }
 
   obscure_address() {
